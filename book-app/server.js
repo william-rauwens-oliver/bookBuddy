f36308c75c63a5db51c044bb426ee6077dcff95d
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-05-22 09:19:19 +0200

    Add Server Data.

---
+++ book-app/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://localhost:27017/my_auth_app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+app.use('/api/auth', require('./routes/auth'));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
