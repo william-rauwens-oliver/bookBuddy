764f12447ea277edb41aaf0d4d8ee6998008acf5
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-05-29 10:16:26 +0200

    Added the ability to favorite a book that does not have a page.

--- book-app/server.js
+++ book-app/server.js
@@ -4,17 +4,17 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const Book = require('./src/models/Book'); // Assurez-vous que le chemin vers votre modèle Book est correct
-const Auth = require('./src/routes/Auth')
+const Auth = require('./src/routes/Auth');
+
 // Initialisation de l'application Express
 const app = express();
-// app.use(bodyParser.json(),Auth);
 
 // Configuration CORS
 app.use(cors({
   origin: ['http://localhost:3000'],
-  methods: ["GET","POST"],
+  methods: ["GET", "POST"],
   credentials: true
-}),bodyParser.json(),Auth);
+}), bodyParser.json(), Auth);
 
 // Port d'écoute du serveur
 const PORT = process.env.PORT || 5000;
@@ -32,19 +32,22 @@ db.once('open', () => {
   console.log('Connecté à MongoDB');
 });
 
-app.get('/toto', async (req,res) => {
+app.get('/toto', async (req, res) => {
   res.status(200).json({ message: 'plop' });
-})
+});
 
 // Route pour ajouter un livre aux favoris
 app.post('/api/favorites', async (req, res) => {
-  console.log(req.body,'toto')
-  // try {
+  console.log(req.body, 'toto');
+  try {
     // Récupération des données du livre à ajouter aux favoris depuis le corps de la requête
-    const { title,author,
-      pages,
-      published } = req.body;
-      if(pages === null) {pages = 1}
+    let { title, author, pages, published } = req.body;
+    
+    // Vérification de la valeur de pages
+    if (!pages) {
+      pages = 1;
+    }
+
     // Création d'une nouvelle instance de livre
     const newBook = new Book({
       title,
@@ -53,26 +56,16 @@ app.post('/api/favorites', async (req, res) => {
       published
     });
 
-    // const newBook = new Book({
-    //   title: title,
-    //   author: authors,
-    //   // categories,
-    //   // pageCount,
-    //   pages: pageCount,
-    //   published : maturityRating
-    // });
-
     // Enregistrement du livre dans la base de données MongoDB
-    await newBook.save(newBook);
-    // console.log(toto,'titi')
+    await newBook.save();
+
     // Réponse réussie
     res.status(201).json({ message: 'Livre ajouté aux favoris avec succès' });
-  // } catch (error) {
+  } catch (error) {
     // Gestion des erreurs
-    // console.error('Erreur lors de l\'ajout du livre aux favoris', error);
-    // res.status(500).json({ message: 'Erreur du serveur' });
-    // res.status(200).json({ message: 'plop ' });
-  // }
+    console.error('Erreur lors de l\'ajout du livre aux favoris', error);
+    res.status(500).json({ message: 'Erreur du serveur' });
+  }
 });
 
 // Lancement du serveur
