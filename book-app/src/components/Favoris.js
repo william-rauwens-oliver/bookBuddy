0bb843721a767245c5109738f623075c4f9d443c
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-05-31 22:21:20 +0200

    Add New Design Favorites.

--- book-app/src/components/Favoris.js
+++ book-app/src/components/Favoris.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Card, Container, Row, Col } from 'react-bootstrap';
+import { Card, Container, Row, Col, Spinner } from 'react-bootstrap';
 import './custom.css'; 
 
 const Favoris = () => {
   const [favorites, setFavorites] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchFavorites = async () => {
       try {
         const response = await axios.get('http://localhost:5000/api/favoris');
         setFavorites(response.data);
+        setLoading(false);
       } catch (error) {
         console.error('Erreur lors de la récupération des favoris', error);
       }
@@ -20,23 +22,33 @@ const Favoris = () => {
   }, []);
 
   return (
-    <Container>
-      <h1 className="my-4">Mes Favoris</h1>
-      <Row>
-        {favorites.map((book) => (
-          <Col md={4} key={book._id} className="mb-4">
-            <Card className="h-100 shadow-sm">
-              <Card.Body>
-                <Card.Title>{book.title}</Card.Title>
-                <Card.Text>
-                  <strong>Auteur:</strong> {book.author}<br />
-                  <strong>Pages:</strong> {book.pages}<br />
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+    <Container className="text-center">
+      <h1 className="my-4 text-right">Mes Favoris</h1>
+      {loading ? (
+        <div className="text-center">
+          <Spinner animation="border" role="status">
+            <span className="sr-only">Chargement...</span>
+          </Spinner>
+        </div>
+      ) : (
+        <>
+          <Row>
+            {favorites.map((book) => (
+              <Col key={book._id} className="mb-4">
+                <Card className="h-100 shadow-sm">
+                  <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <Card.Text>
+                      <strong>Auteur:</strong> {book.author}<br />
+                      <strong>Pages:</strong> {book.pages}<br />
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </Container>
   );
 };
